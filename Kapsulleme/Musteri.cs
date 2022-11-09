e8f0b5a31e75211fc2393b77e11240d30234a871
SEM <SEM@DESKTOP-4GKM6HK>
2022-11-09 13:21:32 +0300

    Add project files.

---
+++ Kapsulleme/Musteri.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kapsulleme
+{
+    internal class Musteri
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        
+        public string _KartNo1 { get => KartNo; set => KartNo = 
+                value.Substring(0, 2) + "*************" +  value.Substring
+                (value.Length - 4); }
+        private string KartNo;
+    }
+}
---
+++ Kapsulleme/Program.cs
@@ -0,0 +1,8 @@
+﻿using Kapsulleme;
+
+Musteri m=new Musteri();
+m.Id = 1;
+m.Ad = "Duygu";
+m.Soyad = "Tezcan Kantar";
+m._KartNo1 = "111111";
+Console.WriteLine(m._KartNo1);
